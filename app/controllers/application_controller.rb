ee20e944d19231f9f134db03729cd4eb89b643ee
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-03-24 13:08:52 -0400

    Fin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@ class ApplicationController < Sinatra::Base
   get '/' do
     { message: "Hello world" }.to_json
   end
+  
+  get '/games' do
+    game = Game.all.order(:title).limit(10)
+    game.to_json
+  end
+  
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json
+  end 
 
 end
